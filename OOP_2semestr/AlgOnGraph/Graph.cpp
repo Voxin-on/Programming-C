52474931b779ab5c666b4f0b117dbd4b8d1de5ec
Voxin <alexlogvinenko2007@gmail.com>
2026-03-23 00:53:20 +0800

    Sort files

---
+++ OOP_2semestr/AlgOnGraph/Graph.cpp
@@ -0,0 +1,58 @@
+#include "graph.h"
+#include <iostream>
+#include <queue>
+
+Graph::~Graph(){
+    for (node_iterator it = nodes.begin(); it != nodes.end(); ++it) {
+        delete *it;
+    }
+}
+
+void Graph::removeNode(Node* node) {
+    nodes.erase(node);
+    // Remove also from all neighbours list
+    for (std::set<Node*>::iterator it = nodes.begin();
+    it != nodes.end(); it++) {
+        (*it)->removeNeighbour(node);
+    }
+}
+
+void Graph::addEdge(Node* begin, Node* end) {
+    if (nodes.find(begin) == nodes.end())
+        return;
+    if (nodes.find(end) == nodes.end())
+        return;
+    begin->addNeighbour(end);
+    end->addNeighbour(begin);
+}
+
+
+bool BFS::connected(Node* begin, Node* end) {
+    std::queue<Node*> nodes; nodes.push(begin);
+    std::set<Node*> visited;
+    while (!nodes.empty()) {
+        Node* next = nodes.front(); nodes.pop();
+        if (end == next) return true;
+        visited.insert(next);
+        for (node_iterator it = next->nb_begin();
+            it != next->nb_end(); it++)
+            if (visited.find(*it) == visited.end())
+                nodes.push(*it);
+    }
+    return false;
+}
+
+bool DFS::connected(Node* begin, Node* end) {
+    visited.clear(); return connected(begin, end, 0);
+}
+    bool DFS::connected(Node* begin, Node* end, int depth) {
+    if (begin == end) return true;
+    visited.insert(begin);
+    for (node_iterator it = begin->nb_begin();
+        it != begin->nb_end(); it++) {
+        if (visited.find(*it) == visited.end()) {
+            if (connected(*it, end, depth + 1)) return true;
+        }
+    }
+    return false;
+}
