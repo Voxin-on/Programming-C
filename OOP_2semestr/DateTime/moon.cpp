ce0177170c6bfec78383992c9d5892d711706ac8
Voxin <alexlogvinenko2007@gmail.com>
2026-04-13 11:51:13 +0800

    Add binary search for moon task

--- OOP_2semestr/DateTime/moon.cpp
+++ OOP_2semestr/DateTime/moon.cpp
@@ -21,8 +21,45 @@ int main(){
         return 1;
     }
 
+    char check;
+    file>>check;
+    bool typeUsual = check=='"' ? 0:1;
+
     clock_t start = clock();
 
+    file.ignore(1000, '\n');
+
+    long long lineStart = file.tellg();
+    file.ignore(1000, '\n');
+    long long lineLen = file.tellg() - lineStart;
+
+    file.seekg(0, ios::end);
+    long long fileSize = file.tellg();
+    long long totalLines = (fileSize - lineStart) / lineLen;
+
+    long long lo = 0, hi = totalLines - 1;
+    while (lo < hi) {
+        long long mid = (lo + hi) / 2;
+        file.seekg(lineStart + mid * lineLen);
+        file.ignore(1000, '\n');
+        int ymd;
+        file >> ymd;
+        int fy = ymd/10000, fm = (ymd/100)%100, fd = ymd%100;
+        DateTime cur(fy, fm, fd);
+        if (cur < target) lo = mid + 1;
+        else hi = mid;
+    }
+    
+    while (lo > 0) {
+        file.seekg(lineStart + (lo - 1) * lineLen);
+        file.ignore(1000, '\n');
+        int ymd;
+        file >> ymd;
+        int fy = ymd/10000, fm = (ymd/100)%100, fd = ymd%100;
+        DateTime cur(fy, fm, fd);
+        if (cur < target) break;
+        lo--;
+    }
     file.ignore(1000, '\n');
 
     bool hasRise = false, hasCulmination = false, hasSet = false;
@@ -33,13 +70,16 @@ int main(){
     int ymd, hms;
     double t, r, el, az, fi, lg;
 
-    while (file >> ymd >> hms >> t >> r >> el >> az >> fi >> lg){
+    while (file >> ymd >> hms){
+        if (typeUsual)
+            file >> t >> r >> el >> az >> fi >> lg;
+        else 
+            file >> r >> el >> az >> fi >> lg;
 
         int fy = ymd/10000, fm = (ymd/100)%100, fd = ymd%100;
         DateTime cur(fy, fm, fd);
-
-        if (cur<target) continue;
-        else if (cur>target) break;
+        
+        if (cur>target) break;
 
         int fh = hms/10000, fmin = (hms/100)%100, fsec = hms%100;
         DateTime dt(fy, fm, fd, fh, fmin, fsec);
@@ -64,7 +104,7 @@ int main(){
     }
 
     clock_t end = clock();
-    double time= (double)(end - start) / CLOCKS_PER_SEC;
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
     cout << "Time: " << time << " sec"<<endl;
 
     target.setFormat(DATE_ONLY);
